ae1dbeffa5e2e41e21e33604434859f2ae248967
Jannis R <mail@jannisr.de>
2016-05-13 03:35:04 +0200

    subway :station:

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict'
 
-// todo
+module.exports = {
+
+	// http://www.openstreetmap.org/relation/53181
+	, U1:  58767
+	, U2:  58428
+	, U3:  58426
+	, U4:  58429
+	, U5:  58430
+	, U55: 58431
+	, U6:  51942
+	, U7:  58425
+	, U8:  58424
+	, U9:  58423
+
+}
