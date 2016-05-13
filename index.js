0405b9b9e6593d9536751dcb66fbcdebf8a93a1b
Jannis R <mail@jannisr.de>
2016-05-13 17:39:11 +0200

    slow regional trains :bullettrain_side:

--- index.js
+++ index.js
@@ -2,6 +2,53 @@
 
 module.exports = {
 
+	// todo: https://de.wikipedia.org/wiki/Liste_der_Eisenbahnlinien_in_Brandenburg_und_Berlin
+
+	, RE1:  [188380, 187767]
+	, RE2:  2607475
+	, RE3:  2609916
+	, RE4:  2523200
+	, RE5:  2523198
+	, RE6:  90073
+	, RE7:  939026
+	, RE10: 2605122
+	, RE15: 2607476
+	, RE18: [3705112, 2046557]
+
+	, RB10: 1728821 // only one direction
+	, RB11: 2607477
+	, RB12: 3403239
+	, RB13: 3407777
+	, RB14: 377858 // only one direction
+	// todo: RB19?
+	, RB20: 3366342
+	, RB21: 300432 // only one direction
+	, RB22: 3404080 // only one direction
+	, RB23: 3399395
+	, RB24: [5460156, 5460157]
+	, RB25: 5738329
+	, RB26: 78795 // only one direction
+	, RB27: 3393817
+	, RB31: 5372447 // only one direction
+	, RB33: 319621 // only one direction
+	, RB35: 79826 // only one direction
+	, RB36: 3377492
+	, RB43: [4713362, 4713263]
+	, RB45: 2606968
+	, RB46: 1691355
+	, RB49: 3490614
+	, RB51: 414317 // only one direction
+	, RB54: 3399070
+	, RB55: 3366300
+	, RB60: 373297 // only one direction
+	// todo: RB61
+	, RB62: 6080243 // only one direction
+	, RB63: 374358 // only one direction
+	, RB66: 78500 // only one direction
+	, RB91: 79804 // only one direction
+	, RB93: 79819 // only one direction
+	, OE65: 949590 // only one direction
+
 	// http://www.openstreetmap.org/relation/53181
 	, U1:  58767
 	, U2:  58428
