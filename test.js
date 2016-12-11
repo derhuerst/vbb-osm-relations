37a5bd3520ff30129ccfd39def70799861ffc81a
Jannis R <mail@jannisr.de>
2016-12-11 14:49:22 +0100

    rewrite tests :white_check_mark:

--- test.js
+++ test.js
@@ -1,25 +1,34 @@
 #!/usr/bin/env node
 'use strict'
 
-const test   = require('tape')
-const got    = require('got')
-
-
+const queue = require('queue')
+const fetch = require('node-fetch')
+const assert = require('assert')
 
 const data   = require('./index')
 const ids = Object.keys(data)
 	.map((line) => data[line])
 	.reduce((acc, x) => acc.concat(Array.isArray(x) ? x : [x]), [])
 
-test(`relations exist`, (t) => {
-	t.plan(ids.length)
-	ids.forEach((id, i) => {
-
-		got(`http://www.openstreetmap.org/api/0.6/relation/${id}`)
-		.catch((err) => t.fail(err.message))
-		.then((res) => {
-			t.pass(`relation ${id} exists`)
-			if (i === ids.length - 1) t.done()
-		})
+const checkIfRelationExists = (id) =>
+	fetch(`https://www.openstreetmap.org/api/0.6/relation/${id}`)
+	.catch((err) => t.fail(err.message))
+	.then((res) => {
+		if (!res.ok) throw new Error('response not ok at relation ' + id)
 	})
+
+
+
+const test = queue({concurreny: 4})
+test.on('erorr', (err) => {
+	console.error(err)
+	process.exit(1)
 })
+test.on('success', (id) => console.error(id + ' ✓'))
+
+ids.forEach((id) => test.push((cb) =>
+	checkIfRelationExists(id)
+	.then(() => cb(null, id))
+	.catch(cb)
+))
+test.start()
