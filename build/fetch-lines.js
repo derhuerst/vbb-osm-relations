6ee6cf0dcc467d681d3defae888237bce9466327
Jannis R <mail@jannisr.de>
2017-07-17 01:05:01 +0200

    bugfixes :bug:

--- build/fetch-lines.js
+++ build/fetch-lines.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 const queue = require('queue')
-const lines = require('vbb-lines')('all')
+const lines = require('vbb-lines')
 const fs = require('fs')
 
 const {bbox} = require('./helpers')
@@ -39,31 +39,36 @@ const productsByRouteTag = {
 
 const dest = path.join(__dirname, '../lines.json')
 
-queryOverpass(query)
-.then((data) => {
-	const q = queue({concurrency: 2})
-	q.on('error', (err) => console.error(err.message))
+const tokenize = (name) => name.toLowerCase().replace(/\s+/, '')
 
-	const lines = {} // OSM IDs by VBB station ID
-	const resolve = (l) => (cb) => {
-		const name = l.tags.ref.toLowerCase()
-		const product = productsByRouteTag[l.tags.route]
+lines(true, 'all')
+.then((lines) => {
+	return queryOverpass(query)
+	.then((data) => {
+		const q = queue({concurrency: 2})
+		q.on('error', (err) => console.error(err.message))
 
-		for (let l2 of lines) {
-			if (l2.name.toLowerCase() !== l.tags) continue
-			if (product && product !== l2.product) continue
+		const result = {} // OSM IDs by VBB station ID
+		const resolve = (l) => (cb) => {
+			const name = tokenize(l.tags.ref)
+			const product = productsByRouteTag[l.tags.route]
 
-			if (!lines[l2.id]) lines[l2.id] = [l.id]
-			else lines[l2.id].push(l.id)
-			cb()
-			return
+			for (let l2 of lines) {
+				if (tokenize(l2.name) !== name) continue
+				if (product && product !== l2.product) continue
+
+				if (!result[l2.id]) result[l2.id] = [l.id]
+				else result[l2.id].push(l.id)
+				cb()
+				return
+			}
+
+			cb(new Error(`line ${l.id} (${name}) does not match`))
 		}
 
-		cb(new Error(`line ${l.id} (${name}) does not match`))
-	}
-
-	for (let l of data.elements) q.push(resolve(l))
-	q.on('end', () => fs.writeFileSync(dest, JSON.stringify(lines)))
-	q.start()
+		for (let l of data.elements) q.push(resolve(l))
+		q.on('end', () => fs.writeFileSync(dest, JSON.stringify(result)))
+		q.start()
+	})
 })
 .catch(showError)
